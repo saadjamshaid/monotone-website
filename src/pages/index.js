9494e595c5e3b0e5efd99e7742d84ef21fec5aba
Saad Jamshaid <74240393+saad-jamshaid@users.noreply.github.com>
2022-01-23 02:56:26 +0500

    flexbox updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ const IndexPage = () => {
     <main>
       <title>Creative Digital Agency in the making</title>
       <h1 className="h1">Coming Soon</h1>
-      <div className="whiteSpace">
-        <div className="handling">
-          <p className="propLeft">Contact us:</p>
+      <div className="footer">
+        <div className="divider">
+          <p className="contactUs">Contact us:</p>
           <a className="email" href="mailto:contact@montone.agency">contact@monotone.agency</a>
         </div>
-        <p className="propRight">© 2022 Monotone. All Rights Reserved.</p>
+        <p className="divider">© 2022 Monotone. All Rights Reserved.</p>
       </div>
     </main>
   )
