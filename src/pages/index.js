7d9269abe74f4bf4e706bd719bbffee9dce815c0
Saad Jamshaid <74240393+saad-jamshaid@users.noreply.github.com>
2022-01-22 23:42:38 +0500

    update footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,11 @@ const IndexPage = () => {
     <main>
       <title>Creative Agency In The Making</title>
       <h1 className="h1">Coming Soon</h1>
+      <div className="whiteSpace">
+        <p className="propLeft">Contact us:</p>
+        <p className="email">contact@montone.agency</p>
+        <p className="propRight">© 2022 Monotone. All Rights Reserved.</p>
+      </div>
     </main>
   )
 }
