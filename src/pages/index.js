d53ff5ff823c98513ef1fb675a8d854bd7967f47
Saad Jamshaid <74240393+saad-jamshaid@users.noreply.github.com>
2022-01-23 04:30:39 +0500

    player addition

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ const IndexPage = () => {
     <main>
       <title>Creative Digital Agency in the making</title>
       <h1 className="h1">Coming Soon</h1>
+
+      <iframe className="none" src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" allow="autoplay" id="iframeAudio"></iframe> 
+
       <div className="footer">
         <div className="divider">
           <p className="contactUs">Contact us:</p>
