9a12ab5fe3920557ef3c864ca9c2020a694403cb
Saad Jamshaid <74240393+saad-jamshaid@users.noreply.github.com>
2022-01-23 00:36:40 +0500

    background and mailto update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ const links = []
 const IndexPage = () => {
   return (
     <main>
-      <title>Creative Agency In The Making</title>
+      <title>Creative Agency in the making</title>
       <h1 className="h1">Coming Soon</h1>
       <div className="whiteSpace">
         <div className="handling">
-          <p className="propLeft">Contact us: </p>
-          <p className="email"> contact@montone.agency</p>
+          <p className="propLeft">Contact us:</p>
+          <a className="email" href="mailto:contact@montone.agency">contact@monotone.agency</a>
         </div>
         <p className="propRight">© 2022 Monotone. All Rights Reserved.</p>
       </div>
