5b6c9a8cd182b3711b11652fdce2407ebba1af56
Saad Jamshaid <74240393+saad-jamshaid@users.noreply.github.com>
2022-01-23 05:53:42 +0500

    logo addition

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,19 @@ const IndexPage = () => {
   return (
     <main>
       <title>Creative Digital Agency in the making</title>
+
+      <div className="header"></div>
       <h1 className="h1">Coming Soon</h1>
-
       <iframe className="none" src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" allow="autoplay" id="iframeAudio"></iframe> 
-
       <div className="footer">
         <div className="divider">
-          <p className="contactUs">Contact us:</p>
-          <a className="email" href="mailto:contact@montone.agency">contact@monotone.agency</a>
+          <div className="textLeftAlign">         
+             <p className="contactUs">Contact us:</p>
+             <a className="email" href="mailto:contact@montone.agency">contact@monotone.agency</a>
+          </div>
+
         </div>
-        <p className="divider">© 2022 Monotone. All Rights Reserved.</p>
+        <p className="divider, textRightAlign">© 2022 Monotone. All Rights Reserved.</p>
       </div>
     </main>
   )
