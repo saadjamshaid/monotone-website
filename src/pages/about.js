a23e13181fe8371157cbad1de59d4507854a2db2
Saad Jamshaid <saadjamshaid@punch.cool>
2022-05-31 03:08:22 +0500

    About page added

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { Link } from 'gatsby'
+
 import '../css/app.css';
 import '../css/typography.css'
 
